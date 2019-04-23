fce15a89387a89b9756c70d7a9f31f64cbe6fcad
龔泓愷 <40812693+Bourbon0212@users.noreply.github.com>
2019-04-23 21:30:38 +0800

    Resume_0423_onload

--- Resume/index.js
+++ Resume/index.js
@@ -1,3 +1,32 @@
+$(function(){
+  // Name Fade in
+  document.getElementById("loadingTxt").style.opacity = 1;
+  document.getElementById("loadingTxt").style.marginTop = "40vh";
+
+  var img = new Image();
+  img.src = './img/PH03.png'; // find load longest Img
+  img.onload = function() {
+
+    setTimeout(function(){
+      // Name Fade Out
+     document.getElementById("loadingTxt").style.opacity = 0;
+     document.getElementById("loadingTxt").style.marginTop = "39vh";
+
+      // BGDiv
+      document.getElementById("loadingBg").style.transition = '1s';
+      document.getElementById("loadingBg").style.backgroundColor = 'rgba(38, 84, 96, 0)';
+      $('#mdl_progress').hide();
+
+      setTimeout(function(){
+        $('#loadingTxt').css('display','none');
+        $('#loadingTxt').hide();
+      }, 600);
+
+    }, 1000);
+
+  }
+})
+
 $(document).ready(function () {
 
     $('#sidebarCollapse').on('click', function () {
@@ -8,6 +37,7 @@ $(document).ready(function () {
     });
 
 });
+
 $(document).on('click', 'a[href^="#"]', function (event) {
     event.preventDefault();
 
